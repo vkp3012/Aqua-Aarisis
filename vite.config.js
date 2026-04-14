e0a5daf3680243d010d3d24251d7e51ece5514c8
vkp3012 <vivekpatel301295@gmail.com>
2026-04-14 19:39:02 +0530

    css change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  css: {
+    transformer: 'postcss', // use postcss instead of lightningcss
+  },
+  build: {
+    cssMinify: 'esbuild', // or false to disable CSS minification
+  },
   plugins: [react()],
 
   resolve: {
